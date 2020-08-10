5450f1c547fd911fa819782fcc2e4c2be360506d
Yangshun Tay <tay.yang.shun@gmail.com>
2020-08-10 18:26:52 +0800

    misc: improve footer styling

--- components/Footer.js
+++ components/Footer.js
@@ -4,53 +4,55 @@ import styles from './Footer.module.scss';
 
 export default function Footer() {
   return (
-    <footer className={clsx('p-3 p-md-5 mt-5', styles.footer)}>
+    <footer className={clsx('pt-5 pb-5 mt-5', styles.footer)}>
       <div className="container">
-        <div className="row">
-          <div className="col-sm">
-            <h5>Join our community</h5>
+        <div className="row gy-5">
+          <div className="col-lg-3">
+            <h5>Join our Community</h5>
             <input
               className="form-control"
               placeholder="E-mail address"
               type="email"
             />
           </div>
-          <div className="col-sm">
-            <div className="mb-5">
-              <h5>Programs</h5>
-              <ul className="list-unstyled">
-                <li>
-                  <a href="#">Bootcamp</a>
-                </li>
-                <li>
-                  <a href="#">Mentorship</a>
-                </li>
-              </ul>
-            </div>
-            <div className="mb-5">
-              <h5>Contribute</h5>
-              <ul className="list-unstyled">
-                <li>
-                  <a href="#">Volunteer</a>
-                </li>
-                <li>
-                  <a href="#">Donate</a>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <h5>About</h5>
-              <ul className="list-unstyled">
-                <li>
-                  <a href="#">Community Code of Conduct</a>
-                </li>
-                <li>
-                  <a href="#">Terms & Conditions</a>
-                </li>
-              </ul>
+          <div className="col-lg-4 offset-lg-1">
+            <div className="row">
+              <div className="col-lg-12 col-6 mb-5">
+                <h5>Programs</h5>
+                <ul className="list-unstyled">
+                  <li>
+                    <a href="#">Bootcamp</a>
+                  </li>
+                  <li>
+                    <a href="#">Mentorship</a>
+                  </li>
+                </ul>
+              </div>
+              <div className="col-lg-12 col-6 mb-5">
+                <h5>Contribute</h5>
+                <ul className="list-unstyled">
+                  <li>
+                    <a href="#">Volunteer</a>
+                  </li>
+                  <li>
+                    <a href="#">Donate</a>
+                  </li>
+                </ul>
+              </div>
+              <div className="col-lg-12 col-6">
+                <h5>About</h5>
+                <ul className="list-unstyled">
+                  <li>
+                    <a href="#">Community Code of Conduct</a>
+                  </li>
+                  <li>
+                    <a href="#">Terms & Conditions</a>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
-          <div className="col-sm">
+          <div className="col-lg-4">
             <h5>Contact Us</h5>
             <p>
               For general enquiries, please email:
