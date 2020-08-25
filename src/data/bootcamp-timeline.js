d028bf25c8e28eb7e3c1b2b8151950552374d2d8
Yangshun Tay <tay.yang.shun@gmail.com>
2020-08-25 15:25:12 +0800

    chore: content-ify the bootcamp timeline

---
+++ src/data/bootcamp-timeline.js
@@ -0,0 +1,68 @@
+export default [
+  {
+    date: '4 July, 11am (SGT)',
+    title: 'Info Session & Application Opens',
+    description: (
+      <>
+        We will share with you more about the application process, and explain
+        the participants will get to learn. Women interested in the TechLadies
+        Bootcamp #6 should join this meeting. RSVP here.
+      </>
+    ),
+  },
+  {
+    date: 'July - August',
+    title: 'Pre-Bootcamp Workshops & Clinics',
+    description: (
+      <>
+        We have a series of workshops lined up to assist you in completing the
+        technical task. These workshops are open to all women who are keen to
+        learn.
+      </>
+    ),
+  },
+  {
+    date: '15 August',
+    title: 'Application Closes',
+    description: (
+      <>
+        Applications close on 15 August so make sure you submit your completed
+        Technical Task and application form to be considered for the Bootcamp.
+      </>
+    ),
+  },
+  {
+    date: '4 September',
+    title: 'Application results',
+    description: (
+      <>
+        Applicants will be notified of their statuses, on a rolling basis,
+        latest by 4 Sep. You may be contacted if we have questions, so do keep
+        your eyes peeled on your email inbox!
+      </>
+    ),
+  },
+  {
+    date: '11 and 19 September',
+    title: 'Level Up workshops',
+    description: (
+      <>
+        The Bootcamp will start with 2 days of Level Up workshops for chosen
+        participants to balance the set of skills needed. Level Up workshops
+        will take place on 11 Sep and 19 Sep afternoon/evening (attendance is
+        obligatory).
+      </>
+    ),
+  },
+  {
+    date: '21 September - 12 December',
+    title: 'TechLadies Bootcamp #6',
+    description: (
+      <>
+        Starting from the week of 21 Sep participants will need to code
+        15hrs/week, inclusive of attending coding sessions every Sat from 10am
+        to 10pm 😱
+      </>
+    ),
+  },
+];
--- src/pages/timeline.js
+++ src/pages/timeline.js
@@ -4,6 +4,8 @@ import NavLink from '../components/NavLink';
 import Timeline from '../components/Timeline';
 import TimelineItem from '../components/TimelineItem';
 
+import bootcampTimeline from '../data/bootcamp-timeline';
+
 export default function Home() {
   return (
     <div>
@@ -13,51 +15,13 @@ export default function Home() {
       <main>
         <div className="container py-6">
           <Timeline>
-            <TimelineItem>
-              <h3 className="h5 text-red mb-3">4 July, 11am (SGT)</h3>
-              <h4 className="h6 mb-3">Info Session & Application Opens</h4>
-              <p className="pb-3">
-                We will share with you more about the application process, and
-                explain the participants will get to learn. Women interested in
-                the TechLadies Bootcamp #6 should join this meeting. RSVP here.
-              </p>
-            </TimelineItem>
-            <TimelineItem>
-              <h3 className="h5 text-red mb-3">4 July, 11am (SGT)</h3>
-              <h4 className="h6 mb-3">Info Session & Application Opens</h4>
-              <p className="pb-3">
-                We will share with you more about the application process, and
-                explain the participants will get to learn. Women interested in
-                the TechLadies Bootcamp #6 should join this meeting. RSVP here.
-              </p>
-            </TimelineItem>
-            <TimelineItem>
-              <h3 className="h5 text-red mb-3">4 July, 11am (SGT)</h3>
-              <h4 className="h6 mb-3">Info Session & Application Opens</h4>
-              <p className="pb-3">
-                We will share with you more about the application process, and
-                explain the participants will get to learn. Women interested in
-                the TechLadies Bootcamp #6 should join this meeting. RSVP here.
-              </p>
-            </TimelineItem>
-            <TimelineItem>
-              <h3 className="h5 text-red mb-3">4 July, 11am (SGT)</h3>
-              <h4 className="h6 mb-3">Info Session & Application Opens</h4>
-              <p className="pb-3">
-                We will share with you more about the application process, and
-                explain the participants will get to learn. Women interested in
-                the TechLadies Bootcamp #6 should join this meeting. RSVP here.
-              </p>
-            </TimelineItem>
-            <TimelineItem>
-              <h3 className="h5 text-red mb-3">4 July, 11am (SGT)</h3>
-              <h4 className="h6 mb-3">Info Session & Application Opens</h4>
-              <p className="pb-3">
-                We will share with you more about the application process, and
-                explain the participants will get to learn. Women interested in
-                the TechLadies Bootcamp #6 should join this meeting. RSVP here.
-              </p>
-            </TimelineItem>
+            {bootcampTimeline.map(({ date, title, description }) => (
+              <TimelineItem key={title}>
+                <h3 className="h5 text-red mb-3">{date}</h3>
+                <h4 className="h6 mb-3">{title}</h4>
+                <p className="pb-3">{description}</p>
+              </TimelineItem>
+            ))}
           </Timeline>
         </div>
       </main>
