ad87cb37a3c14fbcdbb3b04834144bb51b6dc462
qiannyqian <liang.huiqian@gmail.com>
2020-09-12 14:50:23 +0800

    Deploy Next.js to gh-pages

--- _next/static/development/_buildManifest.js
+++ _next/static/development/_buildManifest.js
@@ -1,1 +1,1 @@
-self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
+self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/our-programs":["static\u002Fchunks\u002Fpages\u002Four-programs.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
