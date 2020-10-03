719970d2872eca7651646eec49fd4bcb2a649d83
Elisha <elisha@learnemy.com>
2020-10-03 23:21:22 +0800

    Completed CoC page

--- src/pages/conduct.js
+++ src/pages/conduct.js
@@ -8,7 +8,92 @@ export default function Home() {
       </Head>
       <main>
         <div className="container py-6">
-          <p>Code of conduct goes here!</p>
+          <h2>Overview</h2>
+          <p>We (the TechLadies leadership team) put forth this code of conduct not because we expect inappropriate behavior, but because we believe in the already exceptional level of respect in the TechLadies community. We believe that articulating our values and accountabilities reinforce that respect and provide us with clear avenues to correct our culture should it ever stray. </p>
+          <p>We commit to enforce and develop this code as our community grows. </p>
+          <p>This Code of Conduct applies to members of the TechLadies leadership team, community members, event attendees, volunteers, sponsors, and partners. It applies to interactions in various areas of our shared space, both online and offline, including all events and programs hosted by TechLadies and shared online spaces (Slack, email, Facebook, Twitter) </p>
+          <h2>TechLadies is not an <i>inclusive</i> community</h2>
+          <p>Well, not in the way you are thinking. We are dedicated to providing a safe and comfortable environment for women, non-binary people, and male allies. We are for-women, but we are not anti-men. We celebrate our unique differences and are united by TechLadies’ vision of seeing more women take part in the tech industry.</p>
+          <p>To do this well, there are certain people that we do not accept. We do not welcome behaviors that exclude, discriminate, or harass others based on their: gender, gender identity and expression, age, sexual orientation, disability, physical appearance, body size, race, ethnicity, nationality, religion, political views, marital status, parental status, socioeconomic status, previous TechLadies’ events/programs attendance or lack of, computing experience or lack of, chosen programming language or tech stack.</p>
+
+          <p>As TechLadies is volunteer-led and has limited resources, we would prefer that people do not abuse the resources provided by TechLadies and/or our sponsors. Such abuse could be in the form of, including but not limited to, theft, hogging resources (such as tickets or refreshments), and falsifying information for personal gains. </p>
+          <p>Basically... we don’t welcome people with inappropriate behaviors. </p>
+          <p>To elaborate on what “excludes, discriminates, or harasses” means, it includes but is not limited to: </p>
+          <ul>
+            <li>violent threats or language directed against another person</li>
+            <li>discriminatory jokes and language</li>
+            <li>sexually explicit or violent material</li>
+            <li>posting (or threatening to post) other people’s personal identifying information (“doxing”)</li>
+            <li>personal insults, especially those using racist or sexist terms</li>
+            <li>unwelcomed sexual attention</li>
+            <li>stalking</li>
+            <li>photography or audio/video recording against reasonable consent</li>
+            <li>sustained disruption of talks or other events</li>
+            <li>inappropriate physical contact</li>
+            <li>advocating for, or encouraging, any of the above behavior</li>
+          </ul>
+          <p>In general, be kind to others. And if someone is asked to stop any harassing behavior, please comply immediately.</p>
+          <h2>Notice it, report it </h2>
+          <p>When something goes wrong—whether it’s a microaggression or an instance of harassment—there are some things you can do to make sure the situation is addressed.</p>
+          <ol>
+            <li><b>In urgent cases:</b> If you believe anyone or yourself is in physical danger, please notify law enforcement first and remove yourself or the victim from the location. If you require immediate support, please reach out to any TechLadies leadership team.</li>
+            <li><b>Most recommended:</b> <a href="https://docs.google.com/forms/d/e/1FAIpQLSenVJX_Z_aQvWAJM6fqJ9qQSGQaQew9IDJAx1t7SHYGk_zF2g/viewform" target="_blank">Report it to us by filling up this form.</a> You can make a report either personally or anonymously, and all reports are confidential. Where we determine that a public statement will need to be made, the identities of all victims and reporters will remain confidential unless those individuals instruct us otherwise. If you are unsure whether the incident is a violation, or whether the space where it happened is covered by this Code of Conduct, we encourage you to still report it. We would much rather have a few extra reports where we take no action, rather than miss a report of an actual violation. We do not look negatively on you if we find the incident is not a violation. </li>
+            <li><b>If the report is about a TechLadies volunteer (that is, a coach, a mentor, a helper):</b> Please use the same report form linked above.</li>
+            <li><b>If the report is about a member of the TechLadies Leadership team (that is, someone listed on the About page):</b> Please email Elisha directly at elisha@techladies.co with the following details. We will use the usual enforcement process, excluding the member(s) that the report concerns from accessing the report, joining any discussions, or making any decisions. All reports are confidential.
+              <ul>
+                <li>Names (real, nicknames, or pseudonyms) of any individuals involved. If there were other witnesses besides you, please try to include them.</li>
+                <li>When and where the incident occurred. Please be as specific as possible. </li>
+                <li>Your account of what occurred. If there is a publicly available record (e.g. a screenshot) please include a link. </li>
+                <li>Any extra context you believe existed for the incident. </li>
+                <li>Do you think this incident is ongoing at the time of your report? (Yes/No/I don’t know) </li>
+                <li>Any other information you believe we should have</li>
+              </ul>
+            </li>
+            <li><b>If the report is about the TechLadies founder:</b> Please email anyone listed on the About page with the following details. We will use the usual enforcement process, excluding the member(s) that the report concerns from accessing the report, joining any discussions, or making any decisions. All reports are confidential.
+              <ul>
+                <li>Names (real, nicknames, or pseudonyms) of any individuals involved. If there were other witnesses besides you, please try to include them.</li>
+                <li>When and where the incident occurred. Please be as specific as possible. </li>
+                <li>Your account of what occurred. If there is a publicly available record (e.g. a screenshot) please include a link. </li>
+                <li>Any extra context you believe existed for the incident. </li>
+                <li>Do you think this incident is ongoing at the time of your report? (Yes/No/I don’t know) </li>
+                <li>Any other information you believe we should have</li>
+              </ul>
+            </li>
+            <li><b>If you feel comfortable:</b> Address it directly. For smaller incidents that might be settled with a brief conversation, you can provide the feedback to the person in question. Please use this approach only if you feel comfortable and confident in holding such a conversation; you do not have to carry the weight of addressing these issues yourself. </li>
+          </ol>
+          <p>If a person engages in a behavior that we have deemed to have violated the Code of Code, we may take any action we deem appropriate. </p>
+          <p>This includes education on why a behavior violated the Code of Conduct, warning the offender (publicly or privately), having the offender issue an apology (publicly or privately), expulsion from events and programs with no refunds (if applicable), expulsion from shared online spaces, expulsion from volunteering with TechLadies in any capacity or reporting their behavior to local law enforcement.</p>
+          <h2>What happens after you file a report?</h2>
+          <p>If you’ve made an anonymous report, you will not receive any updates or resolution regarding the violation as we cannot reach out to you.</p>
+          <p>If you make a report personally (with your contact details), you will receive an automated email showing that we’ve received your report. Thereafter, an assigned member of the TechLadies leadership team will reach out to you within 24 hours to acknowledge that we have seen your report.</p>
+          <p>Depending on the severity of the offense, we may take up to 1 week to:</p>
+          <ul>
+            <li>find more information if needed</li>
+            <li>meet as a leadership team to review the incident to understand: What happened; If this behavior violated our CoC; Who the bad actor was; Whether this is an ongoing situation, or if there is a threat to anyone’s physical safety</li>
+            <li>Decide on a resolution. Factors affecting our decision includes but is not limited to:
+              <ul>
+                <li>Could this behavior be avoided? </li>
+                <li>Is this a repeat offender?</li>
+                <li>What is this behavior’s impact on the victim? </li>
+                <li>What is this behavior’s impact on the broader TechLadies community?</li>
+              </ul>
+            </li>
+            <li>reply to you with either a resolution or an explanation of why the incident is not yet resolved</li>
+          </ul>
+          <p>Where we determine that a public statement will need to be made, the identities of all victims and reporters will remain confidential unless those individuals instruct us otherwise.</p>
+          <p>Any of the parties directly involved can request a reconsideration of the TechLadies Leadership team’s decision. To make such a request, contact anyone listed on the About page or email Elisha directly at elisha@techladies.co with your request and motivation and we will review the case.</p>
+          <h2>Take care of each other</h2>
+          <p>If you ever witness something that seems misaligned with our values, err on the side of caring for fellow community members. Even if an incident seems minor, reach out to the person affected by it to check-in. We’d also appreciate it if you would speak to a member of the TechLadies leadership team directly to voice your concerns.</p>
+          <h2>Grateful to those who’ve inspired us</h2>
+          <p>This Code of Conduct is made with the generosity of others who created amazing Codes of Conduct and made it possible for us to use their work. In particular, the TechLadies Code of Conduct is inspired by the work of:</p>
+          <ul>
+            <li>Django Project: https://www.djangoproject.com/conduct/reporting/</li>
+            <li>Conference Code of Conduct: https://confcodeofconduct.com/</li>
+            <li>Project Include: https://projectinclude.org/writing_cocs#cover-what-happens-outside-the-workplace</li>
+            <li>Buffer: https://open.buffer.com/code-of-conduct/</li>
+            <li>Geek Feminism: https://geekfeminism.wikia.org/wiki/Code_of_conduct</li>
+            <li>Hack Code of Conduct: https://hackcodeofconduct.org/</li>
+          </ul>
         </div>
       </main>
     </div>
