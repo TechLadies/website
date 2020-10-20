27b3a42ee2c2f4257b0ef7de8f9cfb30892bf967
Vina Melody <vina_melody@yahoo.com>
2020-10-20 20:36:24 +0800

    Send email address to Sendgrid contact

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   trailingSlash: true,
+  serverRuntimeConfig: {
+    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY
+  },
+  publicRuntimeConfig: {
+    // To expose the config to the browser
+    // SENDGRID_API_KEY: process.env.SENDGRID_API_KEY
+  }
 };
