668dbced90a7936c761baa719c0e6f29977ec594
Vina Melody <vina_melody@yahoo.com>
2020-11-15 10:13:50 +0800

    Extract to HeadComponent

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,13 @@
-import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
+import HeadComponent from "../components/Head"
+import Footer from "../components/Footer"
+import Navbar from "../components/Navbar"
 
 import "../styles/globals.scss";
 
-function MyApp({ Component, pageProps }) {
+function MyApp ({ Component, pageProps }) {
   return (
     <>
+      <HeadComponent />
       <Navbar />
       <Component {...pageProps} />
       <Footer />
