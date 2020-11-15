8e0c7b55420174eaba16302a5c2c7afb3966ef8b
Vina Melody <vina_melody@yahoo.com>
2020-11-15 20:32:46 +0800

    Add GA & FB pixel

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,25 @@
 import HeadComponent from "../components/Head"
 import Footer from "../components/Footer"
 import Navbar from "../components/Navbar"
+import { useAnalytics } from "../hooks/useAnalytics"
+import Router, { useRouter } from "next/router"
+import withFBQ from "next-fbq"
 
 import "../styles/globals.scss";
+import { useEffect } from "react";
 
 function MyApp ({ Component, pageProps }) {
+  const { init, trackPageViewed } = useAnalytics()
+  const router = useRouter()
+
+  useEffect(() => {
+    init("UA-71348167-1")
+    trackPageViewed()
+    router.events.on("routeChangeComplete", () => {
+      trackPageViewed()
+    })
+  }, [router.events])
+
   return (
     <>
       <HeadComponent />
@@ -15,4 +30,5 @@ function MyApp ({ Component, pageProps }) {
   );
 }
 
-export default MyApp;
+// Wrap with fb pixel
+export default withFBQ("2039215636185492", Router)(MyApp)
