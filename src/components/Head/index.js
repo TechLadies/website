b76d641ca84f39b4f352a4eb83061ba040289590
Elisha <elisha@learnemy.com>
2020-12-06 23:08:20 +0800

    Added favicons

--- src/components/Head/index.js
+++ src/components/Head/index.js
@@ -10,6 +10,8 @@ function HeadComponent (props) {
       <meta content="TechLadies is a community in Asia that supports women with the network, education, and mentorship for their careers in technical roles." name="description" />
       <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link rel="icon" href="/ogimages/favicon.png" sizes="16x16" type="image/png" />
+
       <DefaultSeo {...SEO} />
     </Head>
   )
