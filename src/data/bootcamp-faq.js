59e3ab0910c72c738c4cdd787a579d266c59ad06
Thomas Chia <thomas.h.chia@googlemail.com>
2021-06-27 18:42:23 +0800

    Bootcamp Fixes (#38)
    * - Adds Register Now link (behind feature flag as not sure if eventbrite is ready).
    - Hides newsletter signup link as it is not working and overtaken by events for Bootcamp 7. Can be unhidden for next bootcamp (but still needs to be fixed).
    - Makes one section gray background, since newsletter section is hidden.
    
    * Changes 'submission app' to 'technical task'.
    
    * - Fixes timeline copy.
    - Adds CTA to timeline.
    
    * Fixes FAQ copy.
    
    * Updates FAQ reference to in-person meetups.
    
    * Fixes thumbnail layout.

--- src/data/bootcamp-faq.js
+++ src/data/bootcamp-faq.js
@@ -22,7 +22,7 @@ export default [
     item: <>What is the success rate of TechLadies Bootcamps?</>,
     details: (
       <>
-        We have conducted 6 TechLadies Bootcamps since launch, training 50 women. 12(?) of our graduates have successfully gotten a software development internship or junior developer roles, with more in non-engineering roles in the tech industry.
+        We have successfully conducted 6 TechLadies Bootcamps since launch, trained 50 women, and many of our graduates have successfully gotten a software development internship or junior developer roles, with more in non-engineering roles in the tech industry.
       </>
     ),
   },
@@ -49,7 +49,7 @@ export default [
     item: <>What is the format of this program?</>,
     details: (
       <>
-        You will go through a hands-on experience and our coaches will be here to guide you. Throughout the week, you are expected to work on the project with our coaches checking in on your codes and answer any questions you have. This will happen online. On Saturdays, we will meet in-person to learn new concepts and revise old ones.
+        You will go through a hands-on experience and our coaches will be here to guide you. Throughout the week, you are expected to work on the project with our coaches checking in on your codes and answer any questions you have. This will happen online. On Saturdays, we will meet online to learn new concepts and revise old ones.
       </>
     ),
   },
