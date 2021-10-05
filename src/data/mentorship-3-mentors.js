de75686f03cc6831514a06a20637c0a1586100cb
Mohan Ramesh <mohan.ramesh@rakuten.com>
2021-10-05 20:39:10 +0800

    update linkedin profile

--- src/data/mentorship-3-mentors.js
+++ src/data/mentorship-3-mentors.js
@@ -4,72 +4,68 @@ export const mentorsData = {
     mentors: [{
       image: '../team/mentorship3/softwareengineering/amanda.png',
       title: 'Amanda Oh',
-      link: '',
+      link: 'https://www.linkedin.com/in/amandaoh/',
       description: (
         <>
           Amanda is a software engineer who transitioned from the finance industry over 4 years ago after joining a coding bootcamp. She is familiar with the struggles that come with career switching and is keen to share what she has learnt. She was a mentor in a previous Techladies bootcamp and is now based in Berlin. She spends most of her time building applications on AWS and less of her time traveling.
         </>
       ),
     },
-    {
-      image: '../team/mentorship3/softwareengineering/angie.png',
-      title: 'Angie Sasmita',
-      link: '',
-      description: (
-        <>
-          I'm a technology consultant who's passionate in building highly usable products for our users. I'm a polyglot with special interest in mobile technologies. I'm also a fervent advocate of Agile practices such as TDD and CI/CD.
-          Diversity and equal opportunity in tech industry is important for me. If you are interested in entering the tech industry but unsure of how, I'm happy to connect!
-        </>
-      ),
-    },
-    {
-      image: '../team/mentorship3/softwareengineering/salonikaur.png',
-      title: 'Saloni Kaur',
-      link: '',
-      description: (
+      {
+        image: '../team/mentorship3/softwareengineering/angie.png',
+        title: 'Angie Sasmita',
+        link: 'https://www.linkedin.com/in/angie-sasmita/',
+        description: (
           <>
-            currently a Software Developer at Wise (TransferWise).
-            6 amazing years of software development experience up her sleeves
-            very passionate about increasing female count in the IT industry.
-            worked on softwares in a variety of industries including government, finance and IOT space.
-            a part of TechLadies for the past 4 years from volunteering at coding workshops to being a Bootcamp coach and now a Mentorship Lead and a mentor.
+            I'm a technology consultant who's passionate in building highly usable products for our users. I'm a polyglot with special interest in mobile technologies. I'm also a fervent advocate of Agile practices such as TDD and CI/CD.
+            Diversity and equal opportunity in tech industry is important for me. If you are interested in entering the tech industry but unsure of how, I'm happy to connect!
           </>
-      ),
-    },
+        ),
+      },
+      {
+        image: '../team/mentorship3/softwareengineering/salonikaur.png',
+        title: 'Saloni Kaur',
+        link: 'https://www.linkedin.com/in/saloni-kaur/',
+        description: (
+          <>
+            Currently a Software Developer at Wise (TransferWise) with 6 amazing years of software development experience up her sleeves.
+            She's very passionate about increasing female count in the IT industry.
+            Saloni has worked on softwares in a variety of industries including government, finance and IOT space.
+            She's a part of TechLadies for the past 4 years from volunteering at coding workshops to being a Bootcamp coach and now a Mentorship Lead and a mentor.
+          </>
+        ),
+      },
       {
         image: '../team/mentorship3/softwareengineering/vanessa.png',
         title: 'Vanessa Cassandra',
-        link: '',
+        link: 'https://www.linkedin.com/in/vanessa-cassandra/',
         description: (
-            <>
-              Vanessa is a front-end developer in a multi-national software consulting company.
-              She spends her free time learning Japanese language and enjoying Japanese entertainment.
-            </>
+          <>
+            Vanessa is a front-end developer in a multi-national software consulting company.
+            She spends her free time learning Japanese language and enjoying Japanese entertainment.
+          </>
         ),
       },
       {
         image: '../team/mentorship3/softwareengineering/aryn.png',
         title: 'Aryn, Yue Lin Choong',
-        link: '',
+        link: 'https://www.linkedin.com/in/ychoong/',
         description: (
-            <>
-              Current job title:   MVP Studio Engineer, NUS Enterprise
-              Past experience: 10 years in payments as firmware developer, 5 years in various startups as Tech Lead
-              Hobbies: Jigsaw Puzzles
-              Fun facts: Learned how to read Japanese manga and novels
+          <>
+            Currently a MVP Studio Engineer, NUS Enterprise with 10 years of experience in payments as firmware developer, 5 years in various startups as Tech Lead.
 
-            </>
+          </>
         ),
       },
       {
         image: '../team/mentorship3/softwareengineering/biju.png',
         title: 'Biju Joseph',
-        link: '',
+        link: 'https://www.linkedin.com/in/bijujosephjacob/',
         description: (
-            <>
-              Biju Joseph Jacob is a passionate techie who works as a Senior Engineering Manager at Grab. He leads a few teams within the Engineering Platform division, focused on building large scale services and infrastructure that is used across all lines of businesses offered by the company (transport, food, payments, insurance, etc). Prior to this, he was the SE Asia Tech Lead for the Derivatives business at Macquarie Group, working on low-latency market-making systems. He has also previously worked in the algo-trading and high-frequency equity trading team at Bank of America Merrill Lynch.
-              During his free time, Biju relishes experimenting with new technology and frequently works on various small-scale personal tech projects. He is also a bit of an adrenaline junkie – and has done the bungy jump, sky dive, swim with sharks, etc. Outside of working hours, Biju enjoys teaching, speaking at tech events, running, and watching cricket.
-            </>
+          <>
+            Biju Joseph Jacob is a passionate techie who works as a Senior Engineering Manager at Grab. He leads a few teams within the Engineering Platform division, focused on building large scale services and infrastructure that is used across all lines of businesses offered by the company (transport, food, payments, insurance, etc).
+            During his free time, Biju relishes experimenting with new technology and frequently works on various small-scale personal tech projects.
+          </>
         ),
       },
     ],
@@ -80,13 +76,11 @@ export const mentorsData = {
       {
         image: '../team/mentorship3/uiux/esther.png',
         title: 'Esther Yip',
-        link: '',
+        link: 'https://www.linkedin.com/in/jingxinyip/',
         description: (
           <>
             Esther is an experience designer from Singapore, building a more caring and inclusive world through the lens of design.
-
             She believes that designing for equity is the only viable way forward in our increasingly complex world. With a varied background in social design, systems engineering and analytics, her design philosophy is grounded in humility, introspection and trust-building, and fuelled by a strategic understanding of the systems around us.
-
             Throughout her years in designing both digital and physical experiences, she takes a collaborative and participatory approach, while building the capacity of communities and organizations towards sustainable impact.
           </>
         ),
@@ -94,11 +88,10 @@ export const mentorsData = {
       {
         image: '../team/mentorship3/uiux/pham.png',
         title: 'Pham Thanh Ha',
-        link: '',
+        link: 'https://www.linkedin.com/in/pham-thanh-ha-6297457b/',
         description: (
           <>
             Ha is a Senior Product Designer at Carousell and is currently designing Trust & Social at Carousell. Previously she worked at ReferralCandy & Keio-CUTE Center (NUS).  She loves tech, innovation & people.
-
             Ha is also a ‘mediocre’ hiphop dancer & figure sketching artist in her past time.
           </>
         ),
@@ -106,16 +99,16 @@ export const mentorsData = {
       {
         image: '../team/mentorship3/uiux/julie.png',
         title: 'Julie',
-        link: '',
+        link: 'https://www.linkedin.com/in/uxjulie/',
         description: (
           <>
             Julie is a UX Researcher passionate about how to study users and develop amazing products, both in the digital and physical world.
 
             Her Specialties:
-            UX Management
-            UX Research
-            Usability research
-            Accessibility
+            UX Management,
+            UX Research,
+            Usability research,
+            Accessibility and
             User interface design
           </>
         ),
@@ -128,7 +121,7 @@ export const mentorsData = {
       {
         image: '../team/mentorship3/projectmanagement/ankita.png',
         title: 'Ankita Dmello',
-        link: '',
+        link: 'https://www.linkedin.com/in/ankitard/',
         description: (
           <>
             Ankita is currently a Product Manager at Wise! She is helping to build out products in different markets in Asia and understand the needs of Wise’s local customers. Before Wise, she lived in London and was a Product Manager for a beauty box called Birchbox. She relocated to Singapore just before the pandemic and has started illustrating in her free time. (P.S. You can check out her comic here: https://www.instagram.com/smallfry.scribbles/ )
@@ -138,7 +131,7 @@ export const mentorsData = {
       {
         image: '../team/mentorship3/projectmanagement/geraldine.png',
         title: 'Geraldine Low',
-        link: 'https://www.linkedin.com/in/annakolenkina',
+        link: 'https://www.linkedin.com/in/geraldinelhy/',
         description: (
           <>
             Geraldine is currently a product manager working at Ninja Van. She has transited from an operations role to a product role. Her hobbies include reading, hiking and playing the violin.
@@ -148,7 +141,7 @@ export const mentorsData = {
       {
         image: '../team/mentorship3/projectmanagement/shweta.png',
         title: 'Shweta Padmanaban',
-        link: '',
+        link: 'https://www.linkedin.com/in/shwetapadmanaban/',
         description: (
           <>
             Shweta is currently leading the food and mart consumer experience product teams at Grab. She started her career in consumer banking and made the switch into tech product management ~5 years ago. She is based in Singapore.
@@ -163,7 +156,7 @@ export const mentorsData = {
       {
         image: '../team/mentorship3/datascience/catherine.png',
         title: 'Catherine Khaw',
-        link: '',
+        link: 'https://www.linkedin.com/in/cathkhaw/',
         description: (
           <>
             Catherine is an evangelist for data and analytics literacy at economy, organisational and individual levels. She is the founder of DNA Capitals which drives competitive advantage for organisations in all industries through taking full advantage of the opportunities which arise from digitization and big data.
@@ -174,7 +167,7 @@ export const mentorsData = {
       {
         image: '../team/mentorship3/datascience/diyanaresh.png',
         title: 'Diya Naresh Rao',
-        link: '',
+        link: 'https://www.linkedin.com/in/diyanaresh/',
         description: (
           <>
             Diya is an experienced Data Science and Analytics professional, with a keen interest in problem solving and integrating business needs with technology. She has worked with different programming languages and reporting tools, across verticals. She loves to read, travel and constantly learn. Passionate about increasing Women representation in STEM she is a strong advocate of D&I initiatives.
@@ -204,7 +197,7 @@ export const mentorsData = {
       {
         image: '../team/mentorship3/datascience/olivia.png',
         title: 'Olivia Tanuwidjaja',
-        link: '',
+        link: 'https://www.linkedin.com/in/olivia-tanuwidjaja-5a56028a/',
         description: (
           <>
             Olivia has accumulated 6+ years of experience on Analytics across various industries and set up, gaining expertise on data instrumentation and solution design, product experimentation and analytics, to data modeling and heuristics. She’s now a Data Analyst at Google, where she leverages analytics and data sciences techniques to fight and prevent abuse on numerous Google products. In her spare time, she shares about Data Analytics techniques and career on Medium.
