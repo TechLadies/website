6ce3b631fd4f02559efafdf1306bc974202f5053
audreyaudz <audz@kon.sg>
2022-07-17 02:13:02 +0800

    update bootcamp link

--- src/data/bootcamp-7-timeline.js
+++ src/data/bootcamp-7-timeline.js
@@ -62,7 +62,7 @@ export default [
     cta: (
       <a
         className="btn btn-primary mb-3"
-        href="https://forms.gle/tDaJRabosnPreaEi7"
+        href="https://forms.gle/5FmKpBvNpgedWGoh9"
         target="blank"
         rel="noopener"
       >
