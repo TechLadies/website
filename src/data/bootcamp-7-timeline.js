e7d186fca5fa8f0544e51bb4044a1dc4005f3972
Deepak Pakhale <deepakcpakhale@gmail.com>
2022-10-16 14:09:42 +0800

    Added speaker details to bootcamp workshop schedule

--- src/data/bootcamp-7-timeline.js
+++ src/data/bootcamp-7-timeline.js
@@ -1,5 +1,17 @@
 export default [
   {
+    speakers: [
+    {
+        speaker_img:"/team/bootcamp8/sushma_patekar.jpg",
+        speaker_name:"Sushma Patekar",
+        speaker_linkedin_url:"https://www.linkedin.com/in/sushama-patekar-71a75b85"
+    },
+    {
+        speaker_img:"/team/bootcamp8/bhagyalakshmi_konda.jpg",
+        speaker_name:"Bhagyalakshmi Konda",
+        speaker_linkedin_url:"https://www.linkedin.com/in/bhagyalakshmi-konda-a4691420b",
+    }
+    ],
     date: "27th August 2022, 10am-2.30pm",
     location: "AWS (Asia Square Tower)",
     title: "Getting Started with HTML, CSS and Javascript",
@@ -13,6 +25,18 @@ export default [
     ),
   },
   {
+    speakers: [
+    {
+        speaker_img:"/team/bootcamp8/rishita_narayan.jpg",
+        speaker_name:"Rishita Narayan",
+        speaker_linkedin_url:"https://www.linkedin.com/in/rishita-narayan/"
+    },
+    {
+        speaker_img:"/team/bootcamp8/pratiksha_parsewar.jpg",
+        speaker_name:"Pratiksha Parsewar",
+        speaker_linkedin_url:"https://www.linkedin.com/in/pratikshasp",
+    }
+    ],
     date: "10th September 2022, 10am-2.30pm",
     location: "Thoughtworks (Cross Street Exchange)",
     title: "Introduction to Git & Database ",
@@ -25,6 +49,23 @@ export default [
     ),
   },
   {
+  speakers: [
+     {
+          speaker_img:"/team/bootcamp8/amit_singh.jpg",
+          speaker_name:"Amit Singh",
+          speaker_linkedin_url:"https://www.linkedin.com/in/amit1510/",
+      },
+  {
+      speaker_img:"/team/bootcamp8/zhengkun_wang.jpg",
+      speaker_name:"Zhengkun Wang",
+      speaker_linkedin_url:"https://www.linkedin.com/in/zhengkun-wang-73040452/",
+  },
+    {
+        speaker_img:"/team/bootcamp8/apurva_karve.jpg",
+        speaker_name:"Apurva Karve",
+        speaker_linkedin_url:"https://www.linkedin.com/in/apurva-karve/"
+    },
+  ],
     date: "24th September 2022, 10am-2.30pm",
     location: "Thoughtworks (Cross Street Exchange)",
     title: "Getting started with Rest API & NodeJS",
@@ -35,6 +76,17 @@ export default [
     ),
   },
   {
+    speakers: [
+      {
+          speaker_img:"/team/bootcamp8/maryana.jpg",
+          speaker_name:"Maryana",
+          speaker_linkedin_url:"https://www.linkedin.com/in/maryanahermawan"
+      },
+      {
+          speaker_img:"/team/bootcamp8/joey_chua.jpg",
+          speaker_name:"Joey Chua",
+          speaker_linkedin_url:"https://www.linkedin.com/in/joeychuasm",
+      }],
     date: "8th October 2022, 10am-2.30pm",
     location: "[TBC] AWS (Asia Square Tower)",
     title: "Application Getting started with ExpressJS & React",
@@ -52,6 +104,18 @@ export default [
     ),
   },
   {
+  speakers: [
+       {
+            speaker_img:"/team/bootcamp8/sowmya_s.jpg",
+            speaker_name:"Sowmya Selvaraj",
+            speaker_linkedin_url:"https://www.linkedin.com/in/amit1510/",
+        },
+    {
+        speaker_img:"/team/bootcamp8/rishita_narayan.jpg",
+        speaker_name:"Rishita Narayan",
+        speaker_linkedin_url:"https://www.linkedin.com/in/rishita-narayan/"
+    },
+    ],
     date: "22nd October 2022, 10am-2.30pm",
     location: "Thoughtworks (Cross Street Exchange)",
     title: "Introduction to Agile & React Level up",
