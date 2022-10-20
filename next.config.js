9b92fea0412725080f7a09e979460128faae5cf0
audreyaudz <audz@kon.sg>
2022-10-20 23:47:56 +0800

    Updated dependencies, fixed redirect errors

--- next.config.js
+++ next.config.js
@@ -7,4 +7,18 @@ module.exports = {
     // To expose the config to the browser
     // SENDGRID_API_KEY: process.env.SENDGRID_API_KEY
   },
+  async redirects() {
+    return [
+      {
+        source: '/bootcamp-7',
+        destination: '/bootcamp',
+        permanent: true,
+      },
+      {
+        source: '/mentorship-3',
+        destination: '/mentorship',
+        permanent: true,
+      },
+    ]
+  },
 }
--- src/pages/bootcamp-7.js
+++
@@ -1,3 +0,0 @@
-export default function Home() {
-  location.replace("/bootcamp");
-}
